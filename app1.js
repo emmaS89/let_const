2085454d8e18ce6620729501a165d49cd1da42ff
sinem atak <ataksinem89@gmail.com>
2021-03-18 20:19:02 -0400

    let and const

---
+++ app1.js
@@ -0,0 +1,47 @@
+// let & const
+
+// Quiz
+// What is the difference between var and let?
+// What is the difference between var and const?
+// What is the difference between let and const?
+// What is hoisting?
+
+// *****************************************************
+
+// Example 1
+
+// What is the difference between var and let?
+
+// var => we can reassign and redeclare the variable
+// let => we can reassign, but not redeclare the variable
+// Var scope => is funtion scope =>  we can access a hoisted variable with var
+// let scope => is block scope
+
+// *******************************************************
+
+// Example 2
+
+// What is the difference between var and const?
+
+// var => we can reassign and redeclare the variable.
+// const => we cannot reassign and redeclared
+
+// Var scope => is funtion scope =>  we can access a hoisted variable with var
+// const scope => is block scope
+
+// **********************************************************
+
+// Example 3
+// What is the difference between let and const?
+
+// let => we can reassign, but not redeclare the variable
+// const => we cannot reassign and redeclare the variable
+
+// ********************************************************
+
+// Example4
+
+// What is hoisting?
+// Hoisting => JS compiler
+// we can access the created var variable before given a value. It's undefined.
+// if we invoked the function declaration before run the code => JS compiler can execute it.
